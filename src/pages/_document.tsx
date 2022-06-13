c0c16c17cfca81f5ed906b0da70f9a60b422e597
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-06-13 10:59:53 -0300

    feat: add italic fonts from google fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ class MyDocument extends Document {
           <link rel='preconnect' href='https://fonts.googleapis.com' />
           <link rel='preconnect' href='https://fonts.gstatic.com' />
           <link
-            href='https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap'
+            href='https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&display=swap'
             rel='stylesheet'
           />
         </Head>
