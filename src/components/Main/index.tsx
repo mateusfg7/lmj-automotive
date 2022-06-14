84f20d7d1b18c75040d7eabbbd68a1d43e22a3f0
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-06-14 09:52:49 -0300

    feat: update Main component style
    * remove black background
    * remove padding
    * made title bold

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,8 +1,8 @@
 export function Main() {
   return (
-    <main className='bg-[rgba(0,0,0,0.5)] flex-1 flex flex-col justify-center p-20'>
+    <main className='flex-1 flex flex-col justify-center'>
       <div className='w-2/5 text-2xl'>
-        <h1 className='font-normal text-5xl mb-20'>
+        <h1 className='font-bold text-5xl mb-20'>
           Seu problema, nossa especialidade!
         </h1>
         <p className='italic'>
