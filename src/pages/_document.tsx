cc92ff639e7c227027d85089ba0ff5eb3bbaad5b
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-06-14 10:49:24 -0300

    feat: add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel='icon' href='/favicon.png' />
           <link rel='preconnect' href='https://fonts.googleapis.com' />
           <link rel='preconnect' href='https://fonts.gstatic.com' />
           <link
