4feb3d9660c1d6cc1643f6d2484fdb7cf46aaa17
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-06-14 10:25:33 -0300

    feat: update InitialPage colors

--- src/components/InitialPage/index.tsx
+++ src/components/InitialPage/index.tsx
@@ -5,7 +5,7 @@ import { MainSection } from '../MainSection'
 export function InitialPage() {
   return (
     <div className='h-screen text-white bg-[url("/img/background.jpg")] bg-scroll bg-center bg-no-repeat bg-cover'>
-      <div className='bg-[rgba(13,15,20,0.6)] h-full'>
+      <div className='bg-[rgba(5,7,12,0.6)] h-full'>
         <MainSection>
           <Header />
           <Main />
