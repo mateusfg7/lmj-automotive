32215dee53e4ec3b943f618dd5c73ef2601e1e4c
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-06-15 09:58:22 -0300

    feat: make Main responsible

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,14 +1,14 @@
 export function Main() {
   return (
-    <main className='flex-1 flex flex-col justify-center'>
-      <div className='w-2/5 text-2xl'>
-        <h1 className='font-bold text-5xl mb-20'>
+    <main className='flex-1 flex flex-col justify-center lg:justify-start px-7 lg:px-0 lg:pt-28'>
+      <div className='h-3/4 lg:h-auto flex flex-col justify-between text-2xl lg:text-3xl lg:w-1/2 '>
+        <h1 className='font-bold text-4xl lg:text-6xl text-center lg:text-left lg:mb-16'>
           O melhor socorro, na pior hora!
         </h1>
-        <p className='italic text-[#EBEBEB]'>
+        <p className='italic text-justify text-[#EBEBEB] lg:text-left lg:leading-10'>
           Atendendo a <span className='font-normal'>emergências</span> na
           estrada, com o uso de{' '}
-          <span className='font-normal'>métodos e ferramentas inovadores</span>,
+          <span className='font-normal'>métodos e ferramentas portáteis</span>,
           fornecendo um amparo e até o{' '}
           <span className='font-normal'>transporte</span> se necessário.
         </p>
