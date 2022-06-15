5cb0889da66a2993d52147114439502cdb3ca285
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-06-15 10:43:02 -0300

    feat: make AboutUs responsible

--- src/components/AboutUs/index.tsx
+++ src/components/AboutUs/index.tsx
@@ -3,28 +3,22 @@ import { MainSection } from '../MainSection'
 
 export function AboutUs() {
   return (
-    <div id='about-us' className='h-screen text-white bg-[rgb(5,7,12)]'>
+    <div id='about-us' className='lg:h-screen text-white bg-[rgb(5,7,12)]'>
       <MainSection>
-        <div className='w-full h-full flex'>
-          <div className='h-full flex-1 flex flex-col justify-center'>
+        <div className='w-full h-full flex py-12 lg:p-0'>
+          <div className='hidden lg:flex flex-col justify-center w-full h-full flex-1 '>
             <div className='h-[60%] flex flex-col justify-center items-start pr-20'>
-              <h1 className='text-[#CCCFD3] text-3xl py-5 border-b-2 border-[#CCCFD3] duration-500 hover:text-white hover:border-white'>
-                Ética
-              </h1>
-              <h1 className='text-[#CCCFD3] text-3xl py-5 border-b-2 border-[#CCCFD3] duration-500 hover:text-white hover:border-white'>
-                Honestidade
-              </h1>
-              <h1 className='text-[#CCCFD3] text-3xl py-5 border-b-2 border-[#CCCFD3] duration-500 hover:text-white hover:border-white'>
-                Comprometimento
-              </h1>
-              <h1 className='text-[#CCCFD3] text-3xl py-5 border-b-2 border-[#CCCFD3] duration-500 hover:text-white hover:border-white'>
-                Excelência
-              </h1>
+              <h1 className='lmj-value-titles'>Ética</h1>
+              <h1 className='lmj-value-titles'>Honestidade</h1>
+              <h1 className='lmj-value-titles'>Comprometimento</h1>
+              <h1 className='lmj-value-titles'>Excelência</h1>
             </div>
           </div>
-          <div className='h-full flex-1 flex flex-col justify-center'>
-            <div className='h-[60%] flex flex-col justify-between items-start'>
-              <h1 className='text-3xl font-bold italic'>Quem Somos?</h1>
+          <div className='h-full flex-1 flex flex-col justify-center px-7 lg:p-0'>
+            <div className='lg:h-[60%] flex flex-col justify-between items-start'>
+              <h1 className='w-full text-center lg:text-left text-3xl font-bold lg:p-0 mb-14 lg:m-0'>
+                Quem Somos?
+              </h1>
               <p className='text-xl text-justify leading-8'>
                 A <span className='font-normal'>LMJ Automotive</span> é uma
                 empresa do ramo de oficinas mecânicas. Foi fundada no ano de
@@ -33,9 +27,9 @@ export function AboutUs() {
                 o intuito de inovar o mercado de automóveis por meio da
                 implementação de oficinas móveis.
               </p>
-              <div className='w-full flex justify-end'>
+              <div className='w-full flex justify-center lg:justify-end mt-12 lg:m-0'>
                 <Link href='/about-us'>
-                  <a className='w-max duration-300 text-xl font-normal border-white border-2 p-5 rounded-3xl hover:bg-white hover:text-[rgb(5,7,12)]'>
+                  <a className='w-2/3 lg:w-max duration-300 text-center text-xl font-normal border-white border-2 p-5 rounded-xl hover:bg-white hover:text-[rgb(5,7,12)]'>
                     Leia Mais
                   </a>
                 </Link>
