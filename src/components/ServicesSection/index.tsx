b59ae398b3b59f74540a02c7f96873938f59a67d
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-06-27 11:23:42 -0300

    feat: update services section
    * Increase second title size
    * Add vertical padding

--- src/components/ServicesSection/index.tsx
+++ src/components/ServicesSection/index.tsx
@@ -8,22 +8,24 @@ export function ServicesSection() {
   return (
     <div id='services' className='lg:h-screen text-black'>
       <div className='h-full grid grid-areas-services grid-cols-services grid-rows-services'>
-        <section className='grid-in-sec1 bg-white flex flex-col pl-[calc((100vw-85vw)/2)]'>
+        <section className='red-test grid-in-sec1 bg-white flex flex-col pl-[calc((100vw-85vw)/2)] py-5'>
           <header>
             <h1 className='text-3xl font-bold'>Nossos Serviços</h1>
           </header>
-          <div className='flex-1 flex'>
+          <div className='flex-1 flex blue-test py-5'>
             <div className='flex-1'>
-              <h2>Falta de algo no Automóvel</h2>
-              <ul className='list-disc list-inside'>
+              <h2 className='text-2xl font-normal mb-4'>
+                Falta de algo no Automóvel
+              </h2>
+              <ul className='list-disc list-inside text-lg'>
                 <li>Fornecimento de combustível</li>
                 <li>Troca de óleo</li>
                 <li>Troca de refil de água do carro</li>
               </ul>
             </div>
             <div className='flex-1'>
-              <h2>Manutenção Simples</h2>
-              <ul className='list-disc list-inside'>
+              <h2 className='text-2xl font-normal mb-4'>Manutenção Simples</h2>
+              <ul className='list-disc list-inside text-lg'>
                 <li>Carro furou o pneu</li>
                 <li>Bateria descarregou</li>
                 <li>Roda saiu do carro</li>
