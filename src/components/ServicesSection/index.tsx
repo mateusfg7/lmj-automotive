7f2c974040d559a0e44644512dd6c0ed83c82f4c
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-06-29 12:50:27 -0300

    feat: update services list

--- src/components/ServicesSection/index.tsx
+++ src/components/ServicesSection/index.tsx
@@ -8,30 +8,33 @@ export function ServicesSection() {
   return (
     <div id='services' className='lg:h-screen text-black'>
       <div className='h-full grid grid-areas-services grid-cols-services grid-rows-services'>
-        <section className='red-test grid-in-sec1 bg-white flex flex-col pl-[calc((100vw-85vw)/2)] py-5'>
+        <section className='grid-in-sec1 bg-white flex flex-col pl-[calc((100vw-85vw)/2)] py-5'>
           <header>
             <h1 className='text-3xl font-bold'>Nossos Serviços</h1>
           </header>
-          <div className='flex-1 flex blue-test py-5'>
+          <div className='flex-1 flex py-5'>
             <div className='flex-1'>
               <h2 className='text-2xl font-normal mb-4'>
-                Falta de algo no Automóvel
+                Serviço de manutenção
+              </h2>
+              <ul className='list-disc list-inside text-lg'>
+                <li>Troca de pneu</li>
+                <li>Carga de bateria</li>
+                <li>Pane elétrica</li>
+                <li>Troca do aditivo de radiador</li>
+                <li>Troca de Filtros</li>
+                <li>Troca Correia Dentada</li>
+                <li>Troca do Tensor</li>
+              </ul>
+            </div>
+            <div className='flex-1'>
+              <h2 className='text-2xl font-normal mb-4'>
+                Serviços Personalizável
               </h2>
               <ul className='list-disc list-inside text-lg'>
                 <li>Fornecimento de combustível</li>
                 <li>Troca de óleo</li>
                 <li>Troca de refil de água do carro</li>
-              </ul>
-            </div>
-            <div className='flex-1'>
-              <h2 className='text-2xl font-normal mb-4'>Manutenção Simples</h2>
-              <ul className='list-disc list-inside text-lg'>
-                <li>Carro furou o pneu</li>
-                <li>Bateria descarregou</li>
-                <li>Roda saiu do carro</li>
-                <li>Pane elétrica</li>
-                <li>Troca do fluido de freio</li>
-                <li>Aditivo de radiador</li>
               </ul>
             </div>
           </div>
